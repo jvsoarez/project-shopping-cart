9c8777dc85d3152529aa2d1869f0b200d6ca095c
jvsoarez <joaovictorcpsa@gmail.com>
2023-01-29 01:49:59 -0300

    feat: function that does the fetch for the ML API

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,8 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async () => {
+  const URL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  const response = await fetch(URL);
+  const { results } = await response.json();
+  return results;
 };
 
 if (typeof module !== 'undefined') {
