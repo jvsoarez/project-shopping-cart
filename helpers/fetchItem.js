93530f9cf372c6fc7abcc19a221fe31774bfd102
jvsoarez <joaovictorcpsa@gmail.com>
2023-01-30 00:10:23 -0300

    feat: function that does the fetch of the specific item

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,8 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (itemId) => {
+  if (!itemId) return new Error('You must provide an url');
+  const response = await fetch(`https://api.mercadolibre.com/items/${itemId}`);
+  const data = await response.json();
+  return data;
 };
 
 if (typeof module !== 'undefined') {
