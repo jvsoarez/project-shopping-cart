573f9bac9018c4e4a22191a82db9ba451143c4b4
jvsoarez <joaovictorcpsa@gmail.com>
2023-01-31 00:33:45 -0300

    feat: saveCartItems function
    takes an item from the list, puts it in the array and saves it in localStorage
    
    part of the requisite 4

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,5 +1,8 @@
-const saveCartItems = () => {
-  // seu código aqui
+const saveCartItems = (li) => {
+  const itemArray = JSON.parse(localStorage.getItem('cartItems')) || [];
+  const textLi = li.innerText;
+  itemArray.push(textLi);
+  localStorage.setItem('cartItems', JSON.stringify(itemArray));
 };
 
 if (typeof module !== 'undefined') {
